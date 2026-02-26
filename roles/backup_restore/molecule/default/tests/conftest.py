cd9fa2272ca627049e12d4ad5e9bdd0736e45607
Antoine Dupre <antoine.dupre@savoirfairelinux.com>
2026-02-26 17:15:33 +0100

    Add conftest
    Signed-off-by: Antoine Dupre <antoine.dupre@savoirfairelinux.com>

---
+++ roles/backup_restore/molecule/default/tests/conftest.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+import pytest
+
+
+def _scripts():
+    scripts_dir = Path(os.environ["SCRIPTS_SRC_DIR"])
+    return sorted(p.name for p in scripts_dir.iterdir() if p.is_file() and not p.name.startswith("."))
+
+@pytest.fixture(params=_scripts(), ids=str)
+def deployed_script_name(request):
+    return request.param
--- roles/backup_restore/molecule/default/tests/test_default.py
+++ roles/backup_restore/molecule/default/tests/test_default.py
@@ -1,17 +1,9 @@
-import os
 from pathlib import Path
 import pytest
 
 
 DEST_DIR = Path("/usr/local/bin")
 
-def _scripts():
-    scripts_dir = Path(os.environ["SCRIPTS_SRC_DIR"])
-    return sorted(p.name for p in scripts_dir.iterdir() if p.is_file() and not p.name.startswith("."))
-
-@pytest.fixture(params=_scripts(), ids=str)
-def deployed_script_name(request):
-    return request.param
 
 
 def test_scripts_are_present(host, deployed_script_name):
